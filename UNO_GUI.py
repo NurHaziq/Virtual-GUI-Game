b7b78e4536c23d5b247d57004b3db77fb89d32eb
MuhamadAiman <86432251+MuhamadAiman@users.noreply.github.com>
2021-08-06 07:01:27 +0800

    Add Play Button Function
    create playClick when play button is clicked. Make unoDeck card in total 108 cards

--- UNO_GUI.py
+++ UNO_GUI.py
@@ -3,12 +3,21 @@ import UNO_config as UC
 import sys
 import guideNote as GN
 import sqlite3
+from tkinter import messagebox
 
 window = Tk()
 window.title('UNO')
 window.resizable(width = False, height = False)
-window.iconbitmap('C:/gui/UNO_icon.ico')
+window.iconbitmap('C:/Users/user/Desktop/GitHub/Virtual-GUI-Game/GUI Sketch/image/uno_icon.ico')
 
+# Change it to the direction path in ur computer
+#GUI_location = 'D:/UR6523011/Sem 2/VGT123 Technology System Programming II/GUI/GUI_Sketch'
+#card_image_location = '/image/Uno Card'
+card_image_location = '/img/Uno Card'
+GUI_location = 'C:/Users/danis/OneDrive/Desktop/UNO_GUI'
+
+card_image_location = '/img/Uno Card'
+GUI_location = 'C:/Users/user/Desktop/GitHub/Virtual-GUI-Game/GUI Sketch/image'
 
 totalPlayer = None
 
@@ -22,6 +31,9 @@ players = []
 playerNameEntry = []
 playerPassword = []
 playerPasswordEntry = []
+
+unoDeck = UC.buildDeck()
+unoDeck = UC.shuffleDeck(unoDeck, 3)
 
 detailFrame1 = Label(window, bg = 'skyblue2')
 guideInfo = Label(window)
@@ -92,6 +104,115 @@ def rankView():
 
 
 '''
+Button Play when it clicked
+**************************************************
+Parameter       ||  buttonPlay      --> state
+**************************************************
+Return value    ||  detailFrame2    --> Label
+                ||  playerNameEntry --> list
+                ||  playerName      --> list
+                || totalPlayer      --> integer
+'''
+def playClick(buttonPlay):
+    global detailFrame2, gameFrame, pile_card, unoDeck
+
+    countName = 0
+    countPassword = 0
+    lockEntry = 0
+
+    '''
+    Define function for Changing Player Name
+    **************************************************
+    Parameter       ||  None
+    **************************************************
+    Return value    ||  detailFrame2    --> Label
+    ''' 
+    def proceed():
+        msgBox = messagebox.askquestion('Player Naming', 'Do you want to change Player Name or Password ?')
+        if msgBox == 'yes':
+            lockEntry = 0
+        else:
+            lockEntry = 1
+            buttonPlay['state'] = DISABLED
+
+        return lockEntry
+            
+    '''
+    Check last entry name and update the playerName List
+    '''
+    # Save the playerNameEntry into playerName list using for loop
+    for x in range(totalPlayer):
+        playerName[x] = playerNameEntry[x].get().title()
+        playerPassword[x] = playerPasswordEntry[x].get()
+
+    # Check if playerName list is not empty
+    for x in playerName:
+        if x != '':
+            countName += 1
+            
+    for x in playerPassword:
+        if x != '':
+            countPassword += 1
+            
+    if countName == totalPlayer and countPassword == totalPlayer:
+        lockEntry = proceed()
+    
+    # Destroy the detailFrame2
+    detailFrame2.destroy()
+
+    # Recreate detailFrame2
+    detailFrame2 = Label(window, bg = 'skyblue2')
+    detailFrame2.pack(fill = X)
+
+    # clear playerNameEntry list
+    playerNameEntry.clear()
+    playerPasswordEntry.clear()
+
+    '''
+    If in playerName list had empty string it will show Invalid because Player shouldn't had empty name
+    Then if it's has name and update the entry
+    '''
+    for x in range(totalPlayer):
+        Label(detailFrame2, text = f'Player Name {x + 1}', bg = "skyblue2").grid(row = x, column = 0, pady = 5)
+        Label(detailFrame2, text = ':' ,bg = "skyblue2").grid(row = x, column = 1, pady = 5)
+
+        nameEntry = Entry(detailFrame2, width = 20)
+        nameEntry.grid(row = x, column = 2, pady = 5)
+        
+        Label(detailFrame2, text = f'Password {x + 1}', bg = "skyblue2").grid(row = x, column = 3, pady = 5)
+        Label(detailFrame2, text = ':', bg = "skyblue2").grid(row = x, column = 4, pady = 5)
+
+        passwordEntry = Entry(detailFrame2, width = 20, show = "*")
+        passwordEntry.grid(row = x, column = 5, pady = 5)
+
+        if playerName[x] == '' or playerPassword[x] == '':
+            Label(detailFrame2, text = 'Invalid!', fg = 'red', bg = "skyblue2").grid(row = x, column = 6)
+            nameEntry.insert(0, playerName[x])
+            passwordEntry.insert(0, playerPassword[x])
+        
+        elif playerName[x] != '' and playerPassword[x] != '':
+            Label(detailFrame2, text = 'OK!', fg = 'green', bg = "skyblue2").grid(row = x, column = 6)
+
+            nameEntry.insert(0, playerName[x])
+            passwordEntry.insert(0, playerPassword[x])
+            
+            if lockEntry == 1:
+                nameEntry['state'] = DISABLED
+                passwordEntry['state'] = DISABLED
+            
+        playerNameEntry.append(nameEntry)
+        playerPasswordEntry.append(passwordEntry)
+
+    if lockEntry == 1:
+        for player in range(totalPlayer):
+            players.append(UC.drawnCards(unoDeck, 1))
+        for x in range(totalPlayer):
+            
+            pile_card = UC.initializeCard(pile_card, unoDeck)
+        
+
+
+'''
 Button OKAY when it clicked
 *********************************************************
 Parameter       ||  totalPlayerEntry    --> Entry
@@ -150,6 +271,7 @@ def main_page_part_2():
         playerPassword.append('')
         
 
+
 '''
 Define main page for Frame 1
 **************************************
@@ -161,7 +283,7 @@ def main_page_part_1():
     detailFrame1.pack(fill = X)
 
     buttonPlay = Button(detailFrame1, text = 'PLAY', padx = 40, pady = 10,
-        fg = 'WHITE', bg = "blue4", state = DISABLED)
+        fg = 'WHITE', bg = "blue4",command = lambda : playClick (buttonPlay), state = DISABLED)
     buttonPlay.grid(row = 0, column = 0, columnspan = 5, pady = (10, 30))
 
     totalPlayer = Label(detailFrame1, text = 'Total Player', bg = "skyblue2")
@@ -190,5 +312,6 @@ def main_page_part_1():
     historyButton.grid(row = 0, column = 0)
 
 main_page_part_1()
+
 
 window.mainloop()
